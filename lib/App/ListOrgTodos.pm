5683fe73a1f0b1deafc10fa93158a8daff9cf381
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-11-01 08:53:07 +0700

    ListOrgTodos: by default don't show done todos

--- lib/App/ListOrgTodos.pm
+++ lib/App/ListOrgTodos.pm
@@ -21,12 +21,12 @@ our %SPEC;
 my $spec = clone($App::ListOrgHeadlines::SPEC{list_org_headlines});
 $spec->{summary} = "List all todo items in all Org files";
 delete $spec->{args}{todo};
-#$spec->{args}{due_in}[1]{default} = 0;
+$spec->{args}{done}[1]{default} = 0;
 
 $SPEC{list_org_todos} = $spec;
 sub list_org_todos {
     my %args = @_;
-    #$args{due_in} //= 0;
+    $args{done} //= 0;
 
     App::ListOrgHeadlines::list_org_headlines(%args, todo=>1);
 }
