d77abfcb0d0c87b79500b82a183f4f7b82401db6
perlancar (@pc-home) <perlancar@gmail.com>
2015-04-28 12:15:48 +0700

    Add utils: list-org-headlines-fast, list-org-todos-fast, count-org-headlines-fast, count-org-todos-fast

---
+++ lib/App/ListOrgHeadlinesFast.pm
@@ -0,0 +1,85 @@
+package App::ListOrgHeadlinesFast;
+
+# DATE
+# VERSION
+
+use 5.010;
+use strict;
+use warnings;
+
+use App::FilterOrgByHeadlines;
+use Function::Fallback::CoreOrPP qw(clone);
+
+our %SPEC;
+
+$SPEC{':package'} = {
+    v => 1.1,
+    summary => 'List & count Org headlines & todos',
+};
+
+$SPEC{list_org_headlines} = do {
+    my $meta = clone $App::FilterOrgByHeadlines::SPEC{filter_org_by_headlines};
+    $meta->{summary} = 'List Org headlines';
+    delete $meta->{args}{with_content};
+    delete $meta->{args}{with_preamble};
+    delete $meta->{args}{return_array};
+    $meta->{result}{schema} = ['array*', of=>'str*'];
+    $meta;
+};
+sub list_org_headlines {
+    my %args = @_;
+
+    App::FilterOrgByHeadlines::filter_org_by_headlines(
+        %args,
+        with_content => 0,
+        with_preamble => 0,
+        return_array => 1,
+    );
+}
+
+$SPEC{list_org_todos} = do {
+    my $meta = clone $SPEC{list_org_headlines};
+    $meta->{summary} = 'List Org todos';
+    delete $meta->{args}{is_todo};
+    $meta;
+};
+sub list_org_todos {
+    my %args = @_;
+
+    list_org_headlines(
+        %args,
+        is_todo => 1,
+    );
+}
+
+$SPEC{count_org_headlines} = do {
+    my $meta = clone $SPEC{list_org_headlines};
+    $meta->{summary} = 'Count Org headlines';
+    $meta->{result}{schema} = ['int*'];
+    $meta;
+};
+sub count_org_headlines {
+    my %args = @_;
+
+    my $res = list_org_headlines(%args);
+    ~~@$res;
+}
+
+$SPEC{count_org_todos} = do {
+    my $meta = clone $SPEC{count_org_headlines};
+    $meta->{summary} = 'Count Org todos';
+    delete $meta->{args}{is_todo};
+    $meta;
+};
+sub count_org_todos {
+    my %args = @_;
+
+    my $res = count_org_headlines(
+        %args,
+        is_todo => 1,
+    );
+    ~~@$res;
+}
+
+1;
+# ABSTRACT:
