17c79e8a5d43c9f30e4529d79a78cb4888c7db55
perlancar (on pc-home) <perlancar@gmail.com>
2020-04-12 07:02:11 +0700

    Allow adding a reminded date to silence an anniversary

--- lib/App/ListOrgAnniversaries.pm
+++ lib/App/ListOrgAnniversaries.pm
@@ -45,7 +45,7 @@ sub _process_hl {
         }
     }
 
-    my @annivs;
+    my @annivs; # elem = [$field, $date, $date_reminded]
     $hl->walk(
         sub {
             my ($el) = @_;
@@ -54,7 +54,11 @@ sub _process_hl {
                 my $field = $el->field_name;
                 return unless defined($field) &&
                     $field =~ $args->{field_pattern};
-                push @annivs, [$field, $el->datetime];
+                if ($field =~ $args->{reminded_field_pattern} && @annivs) {
+                    $annivs[-1][2] = $el->datetime;
+                } else {
+                    push @annivs, [$field, $el->datetime];
+                }
                 return;
             }
             if ($el->isa('Org::Element::Drawer') && $el->name eq 'PROPERTIES') {
@@ -67,9 +71,15 @@ sub _process_hl {
                                        "must be YYYY-MM-DD");
                         next;
                     }
-                    push @annivs,
-                        [$k, DateTime->new(year=>$1, month=>$2, day=>$3,
-                                       time_zone=>$tz)];
+                    if ($k =~ $args->{reminded_field_pattern} && @annivs) {
+                        $annivs[-1][2] = $el->datetime;
+                    } else {
+                        push @annivs, [
+                            lc $k,
+                            DateTime->new(year=>$1, month=>$2, day=>$3,
+                                          time_zone=>$tz),
+                        ];
+                    }
                     return;
                 }
             }
@@ -77,14 +87,15 @@ sub _process_hl {
     );
 
     if (!@annivs) {
-        log_debug("Node doesn't contain anniversary fields, skipped");
+        log_debug("Headline doesn't contain anniversary fields, skipped");
         return;
     }
-    log_trace("annivs = ", \@annivs);
+
+  ANNIV:
     for my $anniv (@annivs) {
-        my ($field, $date) = @$anniv;
-        log_debug("Anniversary found: field=%s, date=%s",
-                     $field, $date->ymd);
+        my ($field, $date, $date_reminded) = @$anniv;
+        log_debug("Anniversary found: field=%s, date=%s, date reminded=%s",
+                     $field, $date->ymd, $date_reminded ? $date_reminded->ymd : undef);
         my $y = $today->year - $date->year;
         my $date_ly = $date->clone; $date_ly->add(years => $y-1);
         my $date_ty = $date->clone; $date_ty->add(years => $y  );
@@ -98,7 +109,25 @@ sub _process_hl {
                 -$days > $args->{max_overdue};
             next if !defined($args->{due_in}) &&
                 !defined($args->{max_overdue}) &&
-                    DateTime->compare($d, $today) < 0;
+                DateTime->compare($d, $today) < 0;
+
+          REMINDED: {
+                if ($date_reminded) {
+                    my $days_reminded;
+                    $days_reminded = ($date_reminded < $today ? -1:1) *
+                        $date_reminded->delta_days($today)->in_units('days');
+                    last REMINDED if defined($args->{due_in}) &&
+                        $days_reminded > $args->{due_in};
+                    last REMINDED if defined($args->{max_overdue}) &&
+                        -$days_reminded > $args->{max_overdue};
+                    last REMINDED if !defined($args->{due_in}) &&
+                        !defined($args->{max_overdue}) &&
+                        DateTime->compare($date_reminded, $today) < 0;
+                    #log_debug("Anniversary already reminded, skipped");
+                    next DATE;
+                }
+            }
+
             my $pl = abs($days) > 1 ? "s" : "";
             my $hide_age = $date->year == 1900;
             my $msg = sprintf(
@@ -147,17 +176,46 @@ By convention, if year is '1900' it is assumed to mean year is not specified.
 
 By default, all contacts' anniversaries will be listed. You can filter contacts
 using tags ('has_tags' and 'lacks_tags' options), or by 'due_in' and
-'max_overdue' options (due_in=14 and max_overdue=2 is what I commonly use in my
+'max_overdue' options (due_in=14 and max_overdue=7 is what I commonly use in my
 startup script).
+
+If you have acted on someone's birthday or anniversary, you can add another
+field with name: the anniversary field + " reminded" (this is customizable using
+the options `reminded_field_pattern`). For example:
+
+    * First last                              :office:friend:
+      :PROPERTIES:
+      :BIRTHDAY:     1900-06-07
+      :BIRTHDAY_REMINDED:  2020-06-08
+      :EMAIL:        foo@example.com
+      :OTHERFIELD:   ...
+      :END:
+
+or:
+
+    * Some name                               :office:
+      - birthday   :: [1900-06-07 ]
+      - birthday reminded :: [2020-06-09]
+      - email      :: foo@example.com
+      - otherfield :: ...
+
+and the reminder will not be shown again.
 
 _
     args    => {
         %App::OrgUtils::common_args1,
         field_pattern => {
-            summary => 'Field regex that specifies anniversaries',
-            schema  => [str => {
-                default => '(?:birthday|anniversary)',
-            }],
+            summary => 'Regex for fields that specify anniversaries',
+            schema  => 're*',
+            default => qr/(?:birthday|anniversary)/i,
+        },
+        reminded_field_pattern => {
+            schema => 're*',
+            default => qr/reminded/i,
+        },
+        reminded_suffix => {
+            schema => 'str*',
+            default => ' reminded',
         },
         has_tags => {
             summary => 'Filter headlines that have the specified tags',
@@ -218,9 +276,8 @@ sub list_org_anniversaries {
     my $sort  = $args{sort};
     my $tz    = $args{time_zone} // $ENV{TZ} // "UTC";
     my $files = $args{files};
-    my $f     = $args{field_pattern} // '';
-    return [400, "Invalid field_pattern: $@"] unless eval { $f = qr/$f/i };
-    $args{field_pattern} = $f;
+    $args{field_pattern} //= qr/(?:birthday|anniversary)/i;
+    $args{reminded_field_pattern} //= qr/reminded/i;
 
     $today = $args{today} // DateTime->today(time_zone => $tz);
 
